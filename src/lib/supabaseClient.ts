5abfac41b9b536e50e84df368a8f3588e57d8379
Dhiraj <dhirajspj99@gmail.com>
2026-08-10 07:43:35 +0530

    hardcode supabase keys to bypass vercel settings

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT"https://pscjrxinuezsjumybuza.supabase.co "||;
-const supabaseAnonKey = process.env.NEXT_"sb_publishable_rHK6HsACswJM96HNRstYKQ_3MzY_y4b"||;
+// हमने process.env हटा दिया है ताकि Vercel की गलत सेटिंग कोड को क्रैश न कर पाए
+const supabaseUrl = "https://pscjrxinuezsjumybuza.supabase.co";
+const supabaseAnonKey = "sb_publishable_rHK6HsACswJM96HNRstYKQ_3MzY_y4b";
 
-export const supabase = createClient(finalUrl, key);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
