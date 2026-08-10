d8c98de200e18db2d79ade733163a28f3aeb959c
Dhiraj <dhirajspj99@gmail.com>
2026-08-10 07:40:28 +0530

    fix supabase url syntax completely

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT"https://pscjrxinuezsjumybuza.supabase.co "
-const supabaseAnonKey = process.env.NEXT_"sb_publishable_rHK6HsACswJM96HNRstYKQ_3MzY_y4b"
-const finalUrl = url.startsWith('http') ? url : `https://${url}`;
+const supabaseUrl = process.env.NEXT"https://pscjrxinuezsjumybuza.supabase.co "||;
+const supabaseAnonKey = process.env.NEXT_"sb_publishable_rHK6HsACswJM96HNRstYKQ_3MzY_y4b"||;
+
 export const supabase = createClient(finalUrl, key);
