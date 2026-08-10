d420bfe8ea276e4f779e08cc5b3dc728058c40c4
Dhiraj <dhirajspj99@gmail.com>
2026-08-10 07:25:02 +0530

    fix supabase syntax error and secure keys

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = https://pscjrxinuezsjumybuza.supabase.co || '';
-const supabaseAnonKey = sb_publishable_rHK6HsACswJM96HNRstYKQ_3MzY_y4b || '';
+const supabaseUrl = "https://pscjrxinuezsjumybuza.supabase.co "
+const supabaseAnonKey = "sb_publishable_rHK6HsACswJM96HNRstYKQ_3MzY_y4b"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
